42d4b9e9497b9e0962243c8ce2b7f82161409f4f
Megan Phillips <megphillips21@gmail.com>
2022-07-12 23:37:14 -0700

    added views, added urls file, updated paths

--- products/views.py
+++ products/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from products.models import Product
+from rest_framework.response import Response
+from products.serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from products import serializers
+
+
+@api_view(['GET', 'POST'])
+def products_list(request):
+    
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many = True)
+        return Response (serializer.data)
+
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT','DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product,pk=pk)
+    if request.method =='GET':
+        serializer = ProductSerializer(product);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
